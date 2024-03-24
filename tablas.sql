505b01f1cb53a56d4542c4bf97fd4e731edff10a
RbnLinares <rbnlinares@gmail.com>
2024-03-24 18:03:12 +0200

    overtime

---
+++ tablas.sql
@@ -0,0 +1,26 @@
+-- CREATE TABLE positive_articles (
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR(255),
+--     url VARCHAR(255),
+--     content TEXT
+-- );
+
+-- CREATE TABLE negative_articles (
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR(255),
+--     url VARCHAR(255),
+--     content TEXT
+-- );
+
+-- CREATE TABLE articles (
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR(255),
+--     url VARCHAR(255),
+--     content TEXT
+-- );
+
+SELECT * FROM articles
+
+
+
+
