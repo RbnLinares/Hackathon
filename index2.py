00cf022339dcb5fac74e4aa3fe096dc18b80c028
RbnLinares <rbnlinares@gmail.com>
2024-03-25 13:50:41 +0200

    Finish Work

--- index2.py
+++ index2.py
@@ -4,6 +4,7 @@ from textblob import TextBlob
 from bs4 import BeautifulSoup
 import psycopg2
 import config
+import random
 
 def insert_article(conn, table_name, article):
     with conn.cursor() as cursor:
@@ -23,7 +24,7 @@ def analyze_sentiment(text):
         return "negative"
     else:
         return "neutral"
-    
+
 def connect_to_database():
     conn = psycopg2.connect(
         dbname=config.DATABASE,
@@ -34,46 +35,50 @@ def connect_to_database():
     )
     return conn
 
-def educative_resource(query):
-    url = "https://www.google.com/search"
-    params = {
-        "q": query,
-    }
-    response = requests.get(url, params=params)
-    if response.ok:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            if href and "educational" in href: "https://www.yadvashem.org/es/education/educational-materials/video-toolbox/hevt-antisemitism.html"
-            print(f"Suggested Page: {href}")
-    else:
-        print(f"Error al buscar recursos educativos: {response.status_code}")
+def educative_resource():
+    recommended_pages = [
+        "https://www.google.com/search?q=basic+information+on+antisemitism",
+        "https://www.google.com/search?q=combating+antisemitism",
+        "https://www.google.com/search?q=latest+on+antisemitism+2024"
+    ]
+    selected_page = random.choice(recommended_pages)
+    print(f"Suggested Page: {selected_page}")
+
 def store_articles_in_database(news_results, api_key):
     newsapi = NewsApiClient(api_key=api_key)
     all_articles = newsapi.get_everything(q='Jews, Israel, Jewish community, Jewish culture, Jewish history, Antisemitism, Holocaust, Nazi',
                                           language='en',
                                           sort_by='relevancy')
     conn = connect_to_database()
+    articles_list = []
     for article in all_articles['articles']:
         try:
             article_response = requests.get(article['url'])
             article_response.raise_for_status() 
             if article_response.ok:
-                print(article_response.ok)
                 soup = BeautifulSoup(article_response.text, 'html.parser')
                 content = soup.get_text()
-                sentiment = analyze_sentiment(article_response.text)
-            if sentiment == "positive":
-                insert_article(conn, "positive_articles", article)
-            elif sentiment == "negative":
-                insert_article(conn, "negative_articles", article)
-            else:
-                insert_article(conn, "positive_articles", article)
+                sentiment = analyze_sentiment(content)
+                articles_list.append((article, sentiment))
         except requests.exceptions.HTTPError as err:
             print(f"Error al descargar el artículo: {err}")
             continue 
 
+    if articles_list:
+        random_article, sentiment = random.choice(articles_list)
+        print(f'Random Article; {random_article["title"]}\nURL: {random_article["url"]}\nSentiment: {sentiment}')
+
+        if sentiment == "positive":
+            insert_article(conn, "positive_articles", random_article)
+        elif sentiment == "negative":
+            insert_article(conn, "negative_articles", random_article)
+        else:
+            insert_article(conn, "neutral_articles", random_article)
+
     conn.close()
-query = "Jews", "Israel", "Jewish community", "Jewish culture", "Jewish history", "Antisemitism", "Holocaust", "Nazi"
+
+    educative_resource()
+
+# query = "Jews, Israel, Jewish community, Jewish culture, Jewish history, Antisemitism, Holocaust, Nazi"
 api_key = "66145059beff4e648d9ec738e0ffb67a"
-store_articles_in_database(None, api_key) 
+store_articles_in_database(None, api_key)
